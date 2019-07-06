76d9edfded7bccb6035f43067e7dd7e3f5f4b53d
Turbo <toan.takahara@gmail.com>
2019-07-06 00:18:38 -0500

    How i Met Your Mother Friend finder

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Friend-Finder
+# FriendFinder
+
+A fun friend finder app. Answer questions in a survey to find your drinking buddy! 
+(Base off How I Met Your Mother)
+
+
+## Instruction
+
+Start the survey by pressing the button on the home page.
+
+Fill in all the answers to find your drinking buddy.
+
+### Screen Shot 
+
+<img width="640" alt="Screen Shot 2019-07-06 at 12 14 32 AM" src="https://user-images.githubusercontent.com/46271986/60751906-80167b80-9f83-11e9-9279-bcfa9c7f5cbb.png">
+<img width="640" alt="Screen Shot 2019-07-06 at 12 15 11 AM" src="https://user-images.githubusercontent.com/46271986/60751907-86a4f300-9f83-11e9-94c1-d08c62b97398.png">
+<img width="640" alt="Screen Shot 2019-07-06 at 12 16 29 AM" src="https://user-images.githubusercontent.com/46271986/60751909-886eb680-9f83-11e9-87aa-ebd2b20fe290.png">
+
+
---
+++ app/data/friend.js
@@ -0,0 +1,85 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Lily Aldrin',
+    profilePic: 'https://media.giphy.com/media/CPhnZRiFF3fe8/giphy.gif',
+    scores: [
+      "2",
+      "1",
+      "3",
+      "4",
+      "3",
+      "2",
+      "1",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: 'Marshall Eriksen ',
+    profilePic: 'https://media.giphy.com/media/R9birtLDTVVxm/giphy.gif',
+    scores: [
+      "3",
+      "2",
+      "1",
+      "5",
+      "2",
+      "1",
+      "4",
+      "3",
+      "2",
+      "4",
+    ]
+  },
+  {
+    name: 'Tedd Mosby',
+    profilePic: 'https://media.giphy.com/media/HtbTQQw1hiiKQ/giphy.gif',
+    scores: [
+      "4",
+      "5",
+      "2",
+      "4",
+      "5",
+      "5",
+      "4",
+      "3",
+      "4",
+      "3",
+    ]
+  },
+  {
+    name: 'Robin Scherbatsky',
+    profilePic: 'https://media.giphy.com/media/nrkTl7Y1uT4yI/giphy.gif',
+    scores: [
+      "3",
+      "1",
+      "5",
+      "2",
+      "1",
+      "2",
+      "3",
+      "5",
+      "1",
+      "1",
+    ]
+  },
+  {
+    name: 'Barney Stinson',
+    profilePic: 'https://media.giphy.com/media/QN6NnhbgfOpoI/giphy.gif',
+    scores: [
+      "5",
+      "1",
+      "1",
+      "2",
+      "1",
+      "5",
+      "3",
+      "2",
+      "2",
+    ]
+  },
+]
+
+module.exports = friendList;
